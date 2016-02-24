d1937131c72ecd3929e5d8e1857f980e482bed5c
Gab-Metzger <gab.metzger@gmail.com>
2016-02-24 17:49:10 +0100

    Display only extension and link

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 var request = require('superagent');
 var authToken = process.env.API_TOKEN;
 
-console.log("API_TOKEN");
-console.log(authToken);
-
-request
-.get('https://api.real-debrid.com/rest/1.0/user?auth_token=' + authToken)
-.set('Accept', 'application/json')
-.end(function(err, res) {
-  if (err) {
-    console.log("ERROR");
-    console.log(err);
-    return err;
-  } else {
-    var data = res.body
-    console.log(data);
-  }
-});
-
 request
 .post('https://api.real-debrid.com/rest/1.0/unrestrict/link')
 .type('form')
@@ -30,6 +13,7 @@ request
     return err;
   } else {
     var data = res.body
-    console.log(data);
+    console.log("EXTENSION : " + data.filename.substr(data.filename.lastIndexOf('.') + 1));
+    console.log("LINK : " +  data.download);
   }
 });
